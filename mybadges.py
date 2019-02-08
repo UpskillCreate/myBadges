fde1a27f81d258d9e8529b847bd013a641c008f8
UpskillCreate <46326618+UpskillCreate@users.noreply.github.com>
2019-02-08 11:12:32 -0500

    Update mybadges.py

--- mybadges.py
+++ mybadges.py
@@ -19,25 +19,27 @@ data = response.text
 soup = BeautifulSoup(data, 'lxml')
  
 # Extracting all the <a> tags whose class name is 'result-title' into a list.
-badges = soup.findAll('em', {'class': 'word-break'})
+badges = soup.findAll('div', {'class': 'cr-standard-grid-item-content__title'})
  
 # Extracting text from the the <a> tags, i.e. class badges.
 print()
-print("Copy all of this text below for use in your resume, CV, or Social Media Profiles: \n")
+print("Copy all of this text below OR copy the text from the badges.txt file on the left sidebar for use in your resume, CV, or Social Media Profiles: \n")
 
 fobj = open('badges.txt', 'a')
-fobj.write(url+"\n")
+#fobj.write(url+"\n")
+fobj.write(url)
 print("\nmy Badges:")
 for badge in badges:
-    print(badge.text)
+    #print(badge.text)
+    print(badge.text.rstrip())
     with open('badges.txt','a') as f: 
-      fobj.write(badge.text+"\n")
+      fobj.write(badge.text.rstrip())
     badgeCount = badgeCount + 1
 
 print()
 print("Total Badges Earned: " + str(badgeCount))
 with open('badges.txt','a') as f:
-  fobj.write("totalBadges: "+str(badgeCount)+"\n")
+  fobj.write("\ntotalBadges: "+str(badgeCount)+"\n")
 print()
 print("How many badges do you have?  https://bit.ly/uc-mybadges\n")
 print("Source: "+url+"\n")
